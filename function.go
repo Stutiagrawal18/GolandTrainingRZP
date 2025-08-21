aaa4498684fd1ad6f125778acb0cc65d1b6a38fd
Stuti Agrawal <stuti.a@razorpay.com>
2025-08-21 17:04:03 +0530

    update

--- function.go
+++ function.go
@@ -5,6 +5,14 @@ import "fmt"
 func add(x int, y int) int { //here we can also do (x, y int) like if we have two or more parameters with same data type, we can omit it writing everytime and just keep it in last
 	return x + y
 }
-func main() {
-	fmt.Println(add(13, 78))
+
+func concat(s1 string, s2 string) string {
+	return s1 + " " + s2
 }
+func main() {
+	//fmt.Println(add(13, 78))
+	fmt.Println(concat("hello", "world"))
+}
+
+//naked return
+//A return statement without arguements returns the named return values. This is known as naked return
